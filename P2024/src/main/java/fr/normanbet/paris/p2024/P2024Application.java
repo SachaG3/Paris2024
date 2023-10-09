92b6abe95942718ec683341b4d70eb3dcd284ea4
Guignard Sacha <113612623+SachaG3@users.noreply.github.com>
2023-10-09 14:26:58 +0200

    ajout spring

---
+++ P2024/src/main/java/fr/normanbet/paris/p2024/P2024Application.java
@@ -0,0 +1,13 @@
+package fr.normanbet.paris.p2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P2024Application.class, args);
+	}
+
+}
---
+++ P2024/src/test/java/fr/normanbet/paris/p2024/P2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.normanbet.paris.p2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
