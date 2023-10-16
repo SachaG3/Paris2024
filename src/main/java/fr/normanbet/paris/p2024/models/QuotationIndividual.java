3a31e8229b3c014daab05f20874db720b66f27ea
valent1ch <valentin.choisnet@caensup.fr>
2023-10-16 08:43:01 +0200

    feat(models): modification Bet Event Quotation_team

--- src/main/java/fr/normanbet/paris/p2024/models/QuotationIndividual.java
+++ src/main/java/fr/normanbet/paris/p2024/models/QuotationIndividual.java
@@ -1,4 +1,17 @@
 package fr.normanbet.paris.p2024.models;
 
-public class QuotationIndividual {
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class QuotationIndividual extends Quotation {
+
+    @ManyToOne
+    private Athlete athlete;
 }
+
+
--- src/main/java/fr/normanbet/paris/p2024/models/Sport.java
+++ src/main/java/fr/normanbet/paris/p2024/models/Sport.java
@@ -1,6 +1,46 @@
 package fr.normanbet.paris.p2024.models;
 
-public class Sport {
+import fr.normanbet.paris.p2024.models.types.DisciplineType;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+public class Sport extends OlympicElement{
+
+    @Column(length = 150)
+    private String name;
+
+    @Column(length = 150)
+    private String description;
+
+    @ManyToOne(optional = true)
+    private Sport parent;
+
+    private boolean individual;
+
+    @OneToMany(mappedBy = "sport",cascade = CascadeType.ALL)
+    private List<Discipline> disciplines=new ArrayList<>();
+
+
+    public void setDisciplinesLines(String lines){
+        String[] disciplinesLines=lines.split("\r\n");
+        for (String line:disciplinesLines){
+            Discipline discipline=new Discipline();
+            String[] fields=line.split(";");
+            discipline.setName(fields[0]);
+            if(fields.length>1){
+                String t=fields[1];
+                discipline.setType(t.equals("H")? DisciplineType.HOMMES:(t.equals("F")?DisciplineType.FEMMES:DisciplineType.MIXTE));
+            }
+            discipline.setSport(this);
+            this.disciplines.add(discipline);
+        }
+    }
 
 }
