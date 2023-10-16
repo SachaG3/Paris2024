7a8ebc25fad0e8de0b31495c3397d7749e31232f
cdecayeux <calvin.decayeux@caensup.fr>
2023-10-16 09:37:40 +0200

    Modification de News et suppression de News_Part

--- src/main/java/fr/normanbet/paris/p2024/models/News.java
+++ src/main/java/fr/normanbet/paris/p2024/models/News.java
@@ -1,23 +1,35 @@
 package fr.normanbet.paris.p2024.models;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import jakarta.persistence.*;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Date;
 
+
+@Entity
 @Getter
 @Setter
 public class News {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(length = 255)
     private String title;
 
+    @Column(length = Integer.MAX_VALUE)
     private String content;
 
-    private Date dateN;
+    private LocalDateTime dateN;
+
+    @ManyToMany(mappedBy = "news")
+    private List<OlympicElement> links=new ArrayList<>();
 }
