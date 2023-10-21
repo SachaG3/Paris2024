cfc340571253f763f4ec6c7be7864200376f3fa0
Sacha Guignard <sacha.guignard@caensup.fr>
2023-10-21 13:31:04 +0200

    feat(controller,model,view and repesitory):
    ajout des derniére actualité

--- src/main/java/fr/normanbet/paris/p2024/controllers/accueilController.java
+++ src/main/java/fr/normanbet/paris/p2024/controllers/accueilController.java
@@ -3,6 +3,7 @@ package fr.normanbet.paris.p2024.controllers;
 import fr.normanbet.paris.p2024.models.News;
 import fr.normanbet.paris.p2024.repositories.NewsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,10 +17,12 @@ public class accueilController {
     private NewsRepository newsRepository;
     @GetMapping("/")
     public String Accueil(Model model){
-        List<News> element1 = newsRepository.findAllByOrderByDateNDesc(PageRequest.of(0, 1));
-        List<News> newsList = newsRepository.findAllByOrderByDateNDesc(PageRequest.of(1, 5));
-        model.addAttribute("newsList", newsList);
-        model.addAttribute("element1", element1);
+        List<News> element1 = newsRepository.findAllByOrderByDateNDesc(PageRequest.of(0, 6));
+        model.addAttribute("element1", element1.get(0));
+        model.addAttribute("element2", element1.get(1));
+        model.addAttribute("element3", element1.get(2));
+        model.addAttribute("element4", element1.get(3));
+        model.addAttribute("element5", element1.get(4));
 
         return("Accueil");
     }
--- src/main/java/fr/normanbet/paris/p2024/repositories/NewsRepository.java
+++ src/main/java/fr/normanbet/paris/p2024/repositories/NewsRepository.java
@@ -1,5 +1,6 @@
 package fr.normanbet.paris.p2024.repositories;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
