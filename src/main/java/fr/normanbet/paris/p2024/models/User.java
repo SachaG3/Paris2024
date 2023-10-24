fb47b5993995c2b8872570c893185b3459747e30
Sacha Guignard <sacha.guignard@caensup.fr>
2023-10-24 23:38:11 +0200

    feat(controller,model,view and repesitory): ajout de la fonction de création de compte et de connexion (juste n'arrive pas à le garde dans userDetails)

--- src/main/java/fr/normanbet/paris/p2024/models/User.java
+++ src/main/java/fr/normanbet/paris/p2024/models/User.java
@@ -2,34 +2,38 @@ package fr.normanbet.paris.p2024.models;
 
 
 import jakarta.persistence.*;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
-@Getter
-@Setter
+@Data
 @Entity
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @Getter
     @Column(length = 50,unique = true)
-    private String login;
+    private String login ="";
 
     @Column(length = 255)
-    private String password;
+    private String password="";
 
     @Column(length = 50)
-    private String firstname;
+    private String firstname="";
 
     @Column(length = 50)
-    private String lastname;
+    private String lastname="";
 
     @Column(length = 255)
-    private String email;
+    private String email="";
 
     @ManyToOne
     private Role role;
@@ -40,5 +44,40 @@ public class User {
     @ManyToMany
     private List<OlympicElement> favorites=new ArrayList<>();
 
+    @Override
+    public String toString() {
+        return login+ " ("+email+")";
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
 
