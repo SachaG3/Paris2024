b34ca4d13e114813eeab6e6de0c195cb277d6b48
valent1ch <valentin.choisnet@caensup.fr>
2023-11-15 08:40:00 +0100

    plus suppression des journalisation ou suppression des sports

--- src/main/java/fr/normanbet/paris/p2024/controllers/sportController.java
+++ src/main/java/fr/normanbet/paris/p2024/controllers/sportController.java
@@ -1,6 +1,8 @@
 package fr.normanbet.paris.p2024.controllers;
 
+import fr.normanbet.paris.p2024.models.Journalisation;
 import fr.normanbet.paris.p2024.models.Sport;
+import fr.normanbet.paris.p2024.repositories.JournalisationRepository;
 import fr.normanbet.paris.p2024.repositories.SportRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
@@ -12,12 +14,16 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
+
 @Controller
 @RequestMapping("/ajouterunsport")
 public class sportController {
 
     @Autowired
     private SportRepository sportRepository;
+
+    @Autowired
+    private JournalisationRepository journalisationRepository;
 
     @GetMapping
     public String showCreateSportForm(Model model) {
@@ -31,6 +37,13 @@ public class sportController {
             String currentUser = getCurrentUser();
             sport.setAddedBy(currentUser);
             sportRepository.save(sport);
+
+            // Ajouter la journalisation
+            Journalisation journalisation = new Journalisation();
+            journalisation.setSport(sport);
+            journalisation.setAddedBy(currentUser);
+            journalisationRepository.save(journalisation);
+
             redirectAttributes.addFlashAttribute("successMessage", "Sport créé avec succès !");
         } catch (Exception e) {
             redirectAttributes.addFlashAttribute("errorMessage", "Une erreur s'est produite lors de la création du sport.");
@@ -39,28 +52,31 @@ public class sportController {
         return "redirect:/ajouterunsport";
     }
 
-    @PostMapping("/journalisation")
-    public String deleteSelectedSports(@RequestParam("selectedSports") List<Long> selectedSports, RedirectAttributes redirectAttributes) {
+    @GetMapping("/journalisation")
+    public String showJournalisation(Model model) {
+        List<Journalisation> journalisations = (List<Journalisation>) journalisationRepository.findAll();
+        model.addAttribute("journalisations", journalisations);
+        return "journalisation";
+    }
+
+    @PostMapping("/journalisation/delete")
+    public String deleteFromJournalisation(@RequestParam("selectedItems") List<Long> selectedItems, RedirectAttributes redirectAttributes) {
         try {
-            // Supprimez les sports en fonction de leurs identifiants
-            sportRepository.deleteAllById(selectedSports);
-            redirectAttributes.addFlashAttribute("successMessage", "Sports supprimés avec succès !");
+            // Supprimez les éléments de la journalisation en fonction de leurs identifiants
+            journalisationRepository.deleteAllById(selectedItems);
+            redirectAttributes.addFlashAttribute("successMessage", "Éléments de la journalisation supprimés avec succès !");
         } catch (Exception e) {
-            redirectAttributes.addFlashAttribute("errorMessage", "Une erreur s'est produite lors de la suppression des sports.");
+            redirectAttributes.addFlashAttribute("errorMessage", "Une erreur s'est produite lors de la suppression des éléments de la journalisation.");
         }
 
         return "redirect:/ajouterunsport/journalisation";
     }
+
 
     private String getCurrentUser() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         return authentication.getName();
     }
 
-    @GetMapping("/journalisation")
-    public String showJournalisation(Model model) {
-        List<Sport> sports = (List<Sport>) sportRepository.findAll();
-        model.addAttribute("sports", sports);
-        return "journalisation";
-    }
+
 }
