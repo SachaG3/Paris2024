10ab72a90d0b922caf00a4f52542fe1293625a6b
valent1ch <valentin.choisnet@caensup.fr>
2023-11-20 17:02:16 +0100

    modification athlete

--- src/main/java/fr/normanbet/paris/p2024/controllers/athleteController.java
+++ src/main/java/fr/normanbet/paris/p2024/controllers/athleteController.java
@@ -1,51 +1,22 @@
 package fr.normanbet.paris.p2024.controllers;
 
 import fr.normanbet.paris.p2024.models.Athlete;
+import fr.normanbet.paris.p2024.models.types.GenreType;
 import fr.normanbet.paris.p2024.repositories.AthleteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
+@RequestMapping("/athletes")
 public class athleteController {
 
     @Autowired
     private AthleteRepository athleteRepository;
-
-    @GetMapping("/ajouterunathlete")
-    public String showCreateAthleteForm(Model model) {
-        model.addAttribute("athlete", new Athlete());
-        return "ajouterunathlete";
-    }
-
-    @PostMapping("/ajouterunathlete")
-    public String createAthlete(Athlete athlete, RedirectAttributes redirectAttributes) {
-        try {
-            String currentUser = getCurrentUser();
-            athlete.setAddedBy(currentUser);
-            athleteRepository.save(athlete);
-            redirectAttributes.addFlashAttribute("successMessage", "Athlète ajouté avec succès !");
-        } catch (Exception e) {
-            redirectAttributes.addFlashAttribute("errorMessage", "Une erreur s'est produite lors de l'ajout de l'athlète.");
-        }
-
-        return "redirect:/ajouterunathlete";
-    }
-
-    private String getCurrentUser() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        return authentication.getName();
-    }
-
-
     @GetMapping("/recherche")
     public String rechercheAthlete(@RequestParam("nomAthlete") String text , Model model) {
         text="%"+text+"%";
@@ -54,12 +25,57 @@ public class athleteController {
         return "/athlete";
     }
 
-    @GetMapping("/athlete")
+    @GetMapping("")
     public String Actualite(Model model) {
         List<Athlete> athleteList = (List<Athlete>) athleteRepository.findAll();
         model.addAttribute("athleteList", athleteList);
 
         return "/athlete";
     }
-}
 
+    @PostMapping("/create")
+    public Athlete createAthlete(@RequestParam String firstname,
+                                 @RequestParam String lastname,
+                                 @RequestParam String birthdate,
+                                 @RequestParam GenreType genre) {
+        Athlete athlete = new Athlete();
+        athlete.setFirstname(firstname);
+        athlete.setLastname(lastname);
+        athlete.setBirthdate(LocalDate.parse(birthdate));
+        athlete.setGenre(genre);
+
+        return athleteRepository.save(athlete);
+    }
+    @PostMapping("/update/{id}")
+    public String updateAthlete(@PathVariable Long id,
+                                 @RequestParam String firstname,
+                                 @RequestParam String lastname,
+                                 @RequestParam String birthdate,
+                                 @RequestParam GenreType genre) {
+        Athlete athlete = athleteRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Athlete not found"));
+
+        athlete.setFirstname(firstname);
+        athlete.setLastname(lastname);
+        athlete.setBirthdate(LocalDate.parse(birthdate));
+        athlete.setGenre(genre);
+
+        athleteRepository.save(athlete);
+        return ("athlete");
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteAthlete(@PathVariable Long id) {
+        athleteRepository.deleteById(id);
+    }
+
+    @GetMapping("/modifierunathlete/{id}")
+    public String modifierAthlete(@PathVariable Long id, Model model) {
+        Athlete athlete = athleteRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Athlete not found"));
+        model.addAttribute("athlete", athlete);
+        return "modifierunathlete";
+    }
+
+
+}
