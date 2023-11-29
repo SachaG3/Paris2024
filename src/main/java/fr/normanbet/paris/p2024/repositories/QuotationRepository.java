5f1ece4edf32cb5ed611351b25537dc55a032d10
cdecayeux <calvin.decayeux@caensup.fr>
2023-11-29 10:40:47 +0100

    ajout code resultat

--- src/main/java/fr/normanbet/paris/p2024/repositories/QuotationRepository.java
+++ src/main/java/fr/normanbet/paris/p2024/repositories/QuotationRepository.java
@@ -1,11 +1,12 @@
 package fr.normanbet.paris.p2024.repositories;
 
 import fr.normanbet.paris.p2024.models.Quotation;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface QuotationRepository {
+public interface QuotationRepository extends JpaRepository<Quotation, Long> {
     List<Quotation> findAll();
 }
