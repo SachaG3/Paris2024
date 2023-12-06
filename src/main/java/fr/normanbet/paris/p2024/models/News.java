411f6dd92f9b2ccd130cb18a6475f6fb1f5366b8
valent1ch <valentin.choisnet@caensup.fr>
2023-12-06 11:45:29 +0100

    format date changer

--- src/main/java/fr/normanbet/paris/p2024/models/News.java
+++ src/main/java/fr/normanbet/paris/p2024/models/News.java
@@ -9,11 +9,14 @@ import lombok.Getter;
 import lombok.Setter;
 import jakarta.persistence.*;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-
+import java.util.Locale;
 
 
 @Entity
@@ -39,7 +42,40 @@ public class News {
     private User author;
 
     @ManyToMany(mappedBy = "news")
-    private List<OlympicElement> links=new ArrayList<>();
+    private List<OlympicElement> links = new ArrayList<>();
 
+
+    public String getFormattedDate() {
+        return dateN.format(DateTimeFormatter.ofPattern("EEEE d MMMM uuuu 'à' HH'h'mm'min'"));
+    }
+
+    public String getFormattedDate2() {
+        return dateN.format(DateTimeFormatter.ofPattern("EEEE d MMMM uuuu"));
+    }
+
+    public String getRelativeFormattedDate() {
+        LocalDateTime now = LocalDateTime.now();
+        LocalDate today = now.toLocalDate();
+        LocalDate yesterday = today.minusDays(1);
+        LocalDate dateOfOperation = dateN.toLocalDate();
+
+        if (dateOfOperation.equals(today)) {
+            // Si l'opération est d'aujourd'hui, affichez l'heure
+            return dateN.format(DateTimeFormatter.ofPattern("HH:mm"));
+        } else if (dateOfOperation.equals(yesterday)) {
+            // Si l'opération est d'hier
+            return "Hier";
+        } else if (dateN.isAfter(now.minusWeeks(1))) {
+            // Si l'opération est dans la semaine passée
+            return dateN.format(DateTimeFormatter.ofPattern("EEEE", Locale.FRANCE));
+        } else {
+            // Pour les dates plus anciennes
+            return dateN.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG).withLocale(Locale.FRANCE));
+        }
+
+    }
 
 }
+
+
+
