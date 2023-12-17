2a94b172657e1427c43fc4747f3f96d88283e1d5
valent1ch <valentin.choisnet@caensup.fr>
2023-12-17 22:54:42 +0100

    fiche athlète

--- src/main/java/fr/normanbet/paris/p2024/controllers/athleteController.java
+++ src/main/java/fr/normanbet/paris/p2024/controllers/athleteController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -66,4 +65,17 @@ public class athleteController {
         model.addAttribute("athlete", athlete);
         return "modifierunathlete";
     }
+
+    @GetMapping("/ficheathlete/{id}")
+    public String afficherFicheAthlete(@PathVariable Long id, Model model) {
+        Athlete athlete = athleteRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Athlete not found with id: " + id));
+
+        model.addAttribute("athlete", athlete);
+
+        return "/athlete/fiche-athlete";
+    }
+
+
+
 }
